0f2ad1c0213d0e74466646aabc1f5e5b6d752436
Your Name <muhammadalmahdi016@gmail.com>
2024-10-24 09:27:32 +0700

    edit

--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -18,7 +18,7 @@ const projects: Project[] = [
     tags: ["React", "Tailwind", "JavaScript"],
     link: "https://utsweb-three.vercel.app/",
     github: "https://github.com/almahdi00/utsweb",
-    image: "img/web1.png"
+    image: "img/Web1.png"
   },
   {
     title: "Portfolio Website",
